4f4ca41838e5aa2188a395efb30cbaafd70442ed
王晓栋 <wangxiaodong@192.168.1.106>
2017-01-20 15:10:19 +0800

    内购代码

---
+++ AppleBuy/AppleBuy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AppleBuy
+//
+//  Created by 王晓栋 on 2017/1/20.
+//  Copyright © 2017年 王晓栋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AppleBuy/AppleBuy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AppleBuy
+//
+//  Created by 王晓栋 on 2017/1/20.
+//  Copyright © 2017年 王晓栋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
